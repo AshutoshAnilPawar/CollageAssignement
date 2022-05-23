b5ac68eaa9b24f4816a650c24384262d00963fb3
Ashutoshpawar <106021299+AshutoshAnilPawar@users.noreply.github.com>
2022-05-23 15:03:42 +0530

    Create 17reverseDigits.c

---
+++ 1st-sem/17reverseDigits.c
@@ -0,0 +1,25 @@
+//Reverse Digit
+//Ashutosh Anil Pawar
+//BCS-1 
+//Roll No : 42
+// Program number : 17
+
+#include<stdio.h>
+
+int main()
+{
+    int n, reverse=0, rem;
+
+    printf("Enter any random number");
+    scanf("%d", &n);
+
+    while(n!=0) 
+    {
+        rem=n%10;
+        reverse=reverse*10+rem;
+        n/=10;
+
+    }
+    printf("Reverse number is %d",reverse);
+    return 0;
+}
