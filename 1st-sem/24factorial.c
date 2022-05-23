e55cf49dc3374cdbc1e2d0986f911cd539feec1f
Ashutoshpawar <106021299+AshutoshAnilPawar@users.noreply.github.com>
2022-05-23 15:14:25 +0530

    Create 24factorial.c

---
+++ 1st-sem/24factorial.c
@@ -0,0 +1,23 @@
+//Ashutosh Anil Pawar
+//BCS -1 
+//Roll No :42
+//Program Number : 24
+//Program for factorial => 5!=5*4*3*2*1
+
+
+// 1. Factorial Program using Loops
+#include<stdio.h> 
+int main()
+{
+    int i,fact=1,number=1;
+    printf("Enter a Number :");
+    scanf("%d",&number); 
+     for(i=1;i<=number;i++){
+         fact=fact*i;
+     }
+
+     printf("Factorial of %d is %d",number, fact);
+
+     return 0;
+
+}
