b61da6ea6ee253a7b8c10d0d74ab92fc562125f6
Ashutoshpawar <106021299+AshutoshAnilPawar@users.noreply.github.com>
2022-05-23 16:28:42 +0530

    Create 012.c

---
+++ 2nd-sem/012.c
@@ -0,0 +1,25 @@
+// Name : Ashutosh Anil Pawar
+// Class : BCS1
+// Roll Number : 42
+
+// Program for data conversion
+// Example of implicit Conversion
+#include<stdio.h>
+int main(){
+    int x=10;  // integer x 
+    char y='a'; // character c 
+
+    // y is implicitely converted ti int. ASCII
+    // value of 'a' is 97
+    x=x+y; 
+
+    // x i simplicitely converted to float 
+    float z = x + 1.0; 
+     
+     printf("x = %d, z = %f", x, z); 
+
+     return 0; 
+}
+
+// output 
+// x = 107, z = 108.000000
